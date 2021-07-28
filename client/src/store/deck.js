9a46d696f2fb56191c194e17daa93c97eb694edb
Olga Thompson <olgatj2007@gmail.com>
2021-07-28 12:08:36 -0400

    deck reducer

---
+++ client/src/store/deck.js
@@ -0,0 +1,13 @@
+// action types
+
+// actions
+
+const deckReducer = (state = {}, action) => {
+  switch(action.type) {
+    //case
+    default:
+      return state;
+  }
+}
+
+export default deckReducer;
