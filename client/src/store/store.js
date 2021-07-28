5a04333f7fc48f197ddc39c2b8823354fe7a3f4e
Olga Thompson <olgatj2007@gmail.com>
2021-07-28 11:40:57 -0400

    redux store setupc

---
+++ client/src/store/store.js
@@ -0,0 +1,26 @@
+import { createStore, combineReducers, applyMiddleware, compose } from '@reduxjs/toolkit';
+import thunk from "redux-thunk";
+let enhancer;
+
+const rootReducer = combineReducers({
+  // reducers
+});
+
+if (process.env.NODE_ENV === "production") {
+  // in production enhancer should only apply thunk middleware
+  enhancer = applyMiddleware(thunk);
+} else {
+  // in development we use redux dev tools and middleware
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
+  // if REDUX DEVTOOLS fails, use compose. apply thunk and logger
+  // as middleware to enhancer
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(rootReducer, preloadedState, enhancer);
+};
+
+export default configureStore;
