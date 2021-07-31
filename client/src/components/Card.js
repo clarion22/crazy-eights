07a6ca7b2e1e2277ff3db72b11daa1d63962edc7
Olga Thompson <olgatj2007@gmail.com>
2021-07-30 22:38:14 -0400

    disable hover effect on selected hard

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,6 +1,6 @@
-import React, { useEffect, CSSProperties } from 'react';
+import React, { useEffect, CSSProperties, useState } from 'react';
 import './styles/board.css';
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes, css } from 'styled-components';
 import { generateUnicode } from '../util/card_logic';
 
 const bounce = keyframes`
@@ -10,28 +10,36 @@ const bounce = keyframes`
 	100% { transform: translateY(0); }
 `;
 
+const hoverMixin = css`
+	height: 200px;
+	background: #f3e5a0;
+	background: -webkit-linear-gradient(top left, #f3e5a0, #c5d8c2);
+	background: -moz-linear-gradient(top left, #f3e5a0, #c5d8c2);
+	background: linear-gradient(to bottom right, #f3e5a0, #c5d8c2);
+	animation: ${bounce} 1s linear infinite;
+	margin: 0;
+`;
+
 function Card({ card, selectCard, selected, gameSession }) {
 	if (!card) return null;
 
 	const CardDiv = styled.div`
 		height: 200px;
 		padding-top: 10px;
-		background-color: white;
+		background-color: ${selected ? '#b7efd6' : 'white'};
 		width: 148px;
 		margin: 10px 10px;
+		transform: ${selected ? 'translateY(-20px)' : ''};
 		&:after {
 			content: ${generateUnicode(card)};
 			font-size: 14.5em;
 			line-height: 0.7;
+			color: ${card.type === 'Diamonds' || card.type === 'Hearts'
+				? 'red'
+				: 'black'};
 		}
 		&:hover {
-			height: 200px;
-			background: #f3e5a0;
-			background: -webkit-linear-gradient(top left, #f3e5a0, #c5d8c2);
-			background: -moz-linear-gradient(top left, #f3e5a0, #c5d8c2);
-			background: linear-gradient(to bottom right, #f3e5a0, #c5d8c2);
-			animation: ${bounce} 1s linear infinite;
-			margin: 0;
+			${selected ? '' : hoverMixin}
 		}
 	`;
 	if (gameSession) {
