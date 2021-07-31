33ef9f47b7c6b1b9312221b4f00419b339654a63
Olga Thompson <olgatj2007@gmail.com>
2021-07-30 20:28:29 -0400

    animation to card deck hover

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,7 +1,14 @@
 import React, { useEffect, CSSProperties } from 'react';
 import './styles/board.css';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { generateUnicode } from '../util/card_logic';
+
+const bounce = keyframes`
+	0%   { transform: translateY(0); }
+	30%  { transform: translateY(-20px); }
+	50%  { transform: translateY(20px); }
+	100% { transform: translateY(0); }
+`;
 
 function Card({ card, selectCard, selected, gameSession }) {
 	if (!card) return null;
@@ -19,8 +26,12 @@ function Card({ card, selectCard, selected, gameSession }) {
 		}
 		&:hover {
 			height: 200px;
-			background-color: lightgreen;
-			margin: 10px 10px;
+			background: #f3e5a0;
+			background: -webkit-linear-gradient(top left, #f3e5a0, #c5d8c2);
+			background: -moz-linear-gradient(top left, #f3e5a0, #c5d8c2);
+			background: linear-gradient(to bottom right, #f3e5a0, #c5d8c2);
+			animation: ${bounce} 1s linear infinite;
+			margin: 0;
 		}
 	`;
 	if (gameSession) {
