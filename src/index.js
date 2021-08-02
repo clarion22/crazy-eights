6de4c5b24936e1f5314e5a8e0c694bf75505239d
Olga Thompson <olgatj2007@gmail.com>
2021-08-01 22:01:21 -0400

    css reset

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'normalize.css';
 import App from './App';
 import configureStore from './store/store';
 import { Provider } from 'react-redux';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== "production") {
-  window.store = store;
-  // window.sessionActions = sessionActions;
+if (process.env.NODE_ENV !== 'production') {
+	window.store = store;
+	// window.sessionActions = sessionActions;
 }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
